b96b1cd8675e8673e63a797e7fda6b664c6eb951
Irene Kwon <irene.haeryun.kwon@gmail.com>
2024-09-13 10:26:38 -0400

    Add hoisting

---
+++ hoisting.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// Hoisting is the process in JavaScript where all variable and constant 
+// "declarations" are placed in memory first
+
+function example1() {
+    // declaration hoisted 🅾️
+    // initialized to undefined 🅾️
+    // can access variable 🅾️
+    return firstName;
+    var firstName = "Irene";
+}
+
+function example2() {
+    // declaration hoisted 🅾️
+    // not initialized ❎
+    // variable can't be accessed ❎
+    return lastName; // TDZ
+    let lastName;
+}
+
+function example3() {
+    // declaration hoisted 🅾️
+    // not initialized ❎
+    // constant can't be accessed ❎
+    return NUM_OF_WEEKS; // TDZ
+    const NUM_OF_WEEKS = 7; // must be initialized
+}
+
+example1(); // undefined
+example2(); // ReferenceError: Cannot access 'lastName' before initialization
+example3(); // ReferenceError: Cannot access 'NUM_OF_WEEKS' before initialization
+
+// question 🙋🏻‍♀️
+// only a variable with var keyword is hoisted
+    // FALSE - ReferenceError doesn't occur if a variable is not declared
